5f6eed07fa1593d654dcdaec4420d82148184fee
Juan Miranda Cárdenas <jmirandav2@gmail.com>
2026-09-06 13:30:22 -0300

    primer commit

---
+++ src/main/java/com/mtisw/be_lab_autofix_lab1/BeLabAutofixLab1Application.java
@@ -0,0 +1,13 @@
+package com.mtisw.be_lab_autofix_lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeLabAutofixLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeLabAutofixLab1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtisw/be_lab_autofix_lab1/BeLabAutofixLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtisw.be_lab_autofix_lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeLabAutofixLab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
